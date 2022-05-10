3bd5fa61b7e9aa19e9e3de541220ec9d5e572cf2
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-05-10 21:57:15 +0300

    add styles for center group btns comp. Footer

--- react-porten-maket/src/Footer.js
+++ react-porten-maket/src/Footer.js
@@ -10,29 +10,33 @@ class Footer extends React.Component {
 		return(
 			<footer className='footer'>
 				<div className='footer__img'>
-					<div className='footer__body containerM'>
-						<div className='footer__body-left'>
-							<div className='footer__body-title'>О магазине</div>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi semper viverra nunc cursus tortor lectus nunc nulla nibh. Egestas amet consectetur vel vitae aliquam dictum suspendisse. Lobortis eget consequat, tellus et et sed turpis. Pretium quisque vitae, amet, porttitor odio ultricies massa pharetra leo. Et ipsum urna fames in sit mi ultrices nisi, nunc.</p>
-						</div>
-						<div className='footer__body-center'>
-							<div className='footer__body-title'>Категории</div>
-							<div className='footer__body-center-btns'>
-								<button className='footer__body-center-btns-btn'>часы</button>
-								<button className='footer__body-center-btns-btn'>браслеты</button>
-								<button className='footer__body-center-btns-btn'>ремни</button>
-								<button className='footer__body-center-btns-btn'>ювелирные изделия</button>
-								<button className='footer__body-center-btns-btn'>ювелирные изделия</button>
+					<div className='containerM'>
+						<div className='footer__body'>
+							<div className='footer__body-left'>
+								<div className='footer__body-title'>О магазине</div>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi semper viverra nunc cursus tortor lectus nunc nulla nibh. Egestas amet consectetur vel vitae aliquam dictum suspendisse. Lobortis eget consequat, tellus et et sed turpis. Pretium quisque vitae, amet, porttitor odio ultricies massa pharetra leo. Et ipsum urna fames in sit mi ultrices nisi, nunc.</p>
+							</div>
+							<div className='footer__body-center'>
+								<div className='footer__body-title'>Категории</div>
+								<div className='footer__body-center-btns'>
+									<span><button type='submit' className='footer__body-center-btns-btn'>часы</button></span>
+									<span><button type='submit' className='footer__body-center-btns-btn'>браслеты</button></span>
+									
+									<span><button type='submit' className='footer__body-center-btns-btn'>ремни</button></span>
+									<span><button type='submit' className='footer__body-center-btns-btn'>ювелирные изделия</button></span>
+									<span><button type='submit' className='footer__body-center-btns-btn'>запонки</button></span>
+								</div>
+							</div>
+							<div className='footer__body-right'>
+								<div className='footer__body-title'>РАссылка</div>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi semper viverra nunc cursus tortor lectus nunc nulla nibh.</p>
+								<div className='footer__body-right-mailUser'>
+									<input className='footer__body-right-input' type='text' placeholder='Ваша почта' aria-label='Email'/>
+									<button className='footer__body-right-btn' type='submit'>ПОДПИСАТЬСЯ</button>
+								</div>
 							</div>
 						</div>
-						<div className='footer__body-right'>
-							<div className='footer__body-title'>РАссылка</div>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi semper viverra nunc cursus tortor lectus nunc nulla nibh.</p>
-							<div className='footer__body-right-mailUser'>
-								<input className='footer__body-right-input' type='text' placeholder='Ваша почта' aria-label='Email'/>
-								<button className='footer__body-right-btn' type='submit'>ПОДПИСАТЬСЯ</button>
-							</div>
-						</div>
+						
 					</div>
 				</div>
 			</footer>
