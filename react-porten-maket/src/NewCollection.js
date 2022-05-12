7ed8e6743bfff0946c434b18aaca09a5dedc6fe4
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-05-12 11:51:09 +0300

    add component NewCollection

---
+++ react-porten-maket/src/NewCollection.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './css/newCollection.css';
+
+
+class NewCollection extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render(){
+		return(
+			<section className='newCollection'>
+			</section>
+		)
+	}
+}
+
+export default NewCollection;
