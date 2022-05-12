957870bd2e50bc3fcd21766b54bb653aa01c123a
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-05-12 14:36:49 +0300

    add styles for component NewCollection

--- react-porten-maket/src/NewCollection.js
+++ react-porten-maket/src/NewCollection.js
@@ -14,15 +14,15 @@ class NewCollection extends React.Component {
 						<h1 className='newCollection__body-left-title titleM'>СЕЗОН 2020/21</h1>
 						<div className='newCollection__body-left-watchVersions'>
 							<ul>
-								<li></li>
-								<li></li>
-								<li></li>
+								<li> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi u</li>
+								<li>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </li>
+								<li>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud </li>
 							</ul>
 						</div>
 					</div>
 					<div className='newCollection__body-right'>
 						<div className='newCollection__body-right-content'>
-							<h2 className='newCollection__body-rightt-content-title titleM'>Новая коллекция</h2>
+							<h2 className='newCollection__body-right-content-title titleM'>Новая коллекция</h2>
 							<button type='submit' className='newCollection__body-right-content-btn'>Каталог</button>
 						</div>
 					</div>
