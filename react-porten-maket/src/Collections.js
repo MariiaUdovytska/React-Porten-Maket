01f4cb5c0f6e01f83bd8097105cecb74b7958910
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-05-13 21:52:34 +0300

    add styles for component Collections

--- react-porten-maket/src/Collections.js
+++ react-porten-maket/src/Collections.js
@@ -14,9 +14,9 @@ class Collections extends React.Component {
 					</div>
 					<div className='collections__body-right'>
 						<div className='collections__body-right-content'>
-							<h2 className='collections__body-right-content-title titleM'>коллекция 2018</h2>
+							<h2 className='titleM'>коллекция 2018</h2>
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non rutrum ornare ut mattis habitant dui arcu. Sagittis amet nunc ut neque quis nibh arcu. Vivamus vestibulum nisi et venenatis sed scelerisque magna consectetur. Amet convallis quis gravida facilisis vulputate. Faucibus facilisi habitasse ipsum interdum dictum aliquet. Velit quis ullamcorper pulvinar nulla malesuada integer. Aenean praesent viverra nulla nullam natoque volutpat curabitur auctor. Viverra viverra ullamcorper scelerisque risus dignissim egestas. Id aliquam a aliquam egestas leo orci pharetra sed diam. </p>
-							<button type='submit' className='collections__body-right-content-btn'>посмотреть коллекцию</button>
+							<button type='submit'>посмотреть коллекцию</button>
 						</div>
 					</div>
 				</div>
