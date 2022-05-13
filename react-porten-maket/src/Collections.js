dec64e8a6f87913804211b383576f79154180aa4
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-05-13 21:37:08 +0300

    add component Collections and html skeleton

---
+++ react-porten-maket/src/Collections.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './css/collections.css';
+
+
+class Collections extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render(){
+		return(
+			<section className='collections'>
+				<div className='collections__body'>
+					<div className='collections__body-left'>
+					</div>
+					<div className='collections__body-right'>
+						<div className='collections__body-right-content'>
+							<h2 className='collections__body-right-content-title titleM'>коллекция 2018</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non rutrum ornare ut mattis habitant dui arcu. Sagittis amet nunc ut neque quis nibh arcu. Vivamus vestibulum nisi et venenatis sed scelerisque magna consectetur. Amet convallis quis gravida facilisis vulputate. Faucibus facilisi habitasse ipsum interdum dictum aliquet. Velit quis ullamcorper pulvinar nulla malesuada integer. Aenean praesent viverra nulla nullam natoque volutpat curabitur auctor. Viverra viverra ullamcorper scelerisque risus dignissim egestas. Id aliquam a aliquam egestas leo orci pharetra sed diam. </p>
+							<button type='submit' className='collections__body-right-content-btn'>посмотреть коллекцию</button>
+						</div>
+					</div>
+				</div>
+			</section>
+		)
+	}
+}
+
+export default Collections;
